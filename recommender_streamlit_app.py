5e28c4b8ac305c3d770b9d572811eb49517faa4f
nishalsach <sachita.nishal@gmail.com>
2022-08-06 21:56:07 -0500

    Start the app

--- recommender_streamlit_app.py
+++ recommender_streamlit_app.py
@@ -1,22 +1,44 @@
 # import module
 import streamlit as st
- 
+import pandas as pd
+from datetime import date
+
+# Read in data
+predictions = pd.read_json(
+    'https://github.com/nishalsach/lead_generation_app/blob/main/predicted_data_sample.json', 
+    orient='records')
+
+# Today's date
+today = date.today()
+# Get relative dates
+two_weeks_ago = today - pd.Timedelta(days=14)
+two_months_ago = today - pd.Timedelta(days=60)
+six_months_ago = today - pd.Timedelta(days=180)
+# Make dictionary of relative dates
+date_dict = {
+    'Last two weeks': two_weeks_ago,
+    'Last two months': two_months_ago,
+    'Last six months': six_months_ago
+}
+
 # Title
 st.title("arXiv Lead Recommender")
 
-st.markdown("#### Select upto 3 news venues you would like to write for: ")
-venues = st.multiselect(
-    "",
-    ['MIT Technology Review', 'Wired', 'VentureBeat'])
+# Time range buttons
+time_range = st.radio(
+    "Select time range for leads", 
+    ("Last two weeks", "Last two months", "Last six months"))
 
+# Start date
+start_date = date_dict[time_range]
 
-# multi select box for venues
+# Multiselect box for venues
  
 # first argument takes the box title
 # second argument takes the options to show
-# st.markdown("#### Select upto 3 news venues you would like to write for: ")
+st.markdown("#### Select upto 3 news venues you would like to write for: ")
 venues = st.multiselect(
-    "#### Select upto 3 news venues you would like to write for: ",
+    "",
     ['MIT Technology Review', 'Wired', 'VentureBeat'])
 
 
