30816324259ee463a8460a80c538a29f144a7cc8
nishalsach <sachita.nishal@gmail.com>
2022-08-06 21:33:50 -0500

    Start the app

--- recommender_streamlit_app.py
+++ recommender_streamlit_app.py
@@ -2,4 +2,14 @@
 import streamlit as st
  
 # Title
-st.title("This is my app!")
+st.title("Lead Recommender")
+
+# multi select box for venues
+ 
+# first argument takes the box title
+# second argument takes the options to show
+venues = st.multiselect(
+    "Select upto 3 news venues you would like to write for: ",
+    ['MIT Technology Review', 'Wired', 'VentureBeat'])
+
+
