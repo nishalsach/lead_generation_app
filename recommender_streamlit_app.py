250eeb8f29cdc565c592f534a77d35dd0391917c
nishalsach <sachita.nishal@gmail.com>
2022-08-06 21:36:38 -0500

    Start the app

--- recommender_streamlit_app.py
+++ recommender_streamlit_app.py
@@ -4,13 +4,19 @@ import streamlit as st
 # Title
 st.title("arXiv Lead Recommender")
 
+st.markdown("#### Select upto 3 news venues you would like to write for: ")
+venues = st.multiselect(
+    "",
+    ['MIT Technology Review', 'Wired', 'VentureBeat'])
+
+
 # multi select box for venues
  
 # first argument takes the box title
 # second argument takes the options to show
-st.markdown("### Select upto 3 news venues you would like to write for: ")
+# st.markdown("#### Select upto 3 news venues you would like to write for: ")
 venues = st.multiselect(
-    "",
+    "#### Select upto 3 news venues you would like to write for: ",
     ['MIT Technology Review', 'Wired', 'VentureBeat'])
 
 
