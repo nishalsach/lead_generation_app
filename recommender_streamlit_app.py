05d3828a05ea773e6ade52b06db1342d01388168
nishalsach <sachita.nishal@gmail.com>
2022-08-07 14:38:59 -0500

    Hello pls work

--- recommender_streamlit_app.py
+++ recommender_streamlit_app.py
@@ -4,9 +4,8 @@ import pandas as pd
 from datetime import date
 
 # Read in data
-predictions = pd.read_json(
-    'predicted_data_sample_latest.json', 
-    orient='records')
+predictions = pd.read_csv(
+    'predicted_data_sample_latest.csv')
 
 st.write(predictions.loc[0, 'published'])
 # Set some variables
